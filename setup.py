8918fff29a18a0f41b4fd065116ae697dde5a3ff
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2015-08-06 05:35:22 +0300

    Bump to v0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='marc2bib',
-    version='0.1.3',
+    version='0.1.4',
     url='https://github.com/mstolyarchuk/marc2bib',
     author='Maxim Stolyarchuk',
     author_email='maxim.stolyarchuk@gmail.com',
