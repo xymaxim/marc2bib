e25a4d7ec90e58c21333bc623a9840332f19d7df
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2022-03-11 03:25:42 +0100

    Add links for the MARC fields

--- marc2bib/tagfuncs.py
+++ marc2bib/tagfuncs.py
@@ -4,9 +4,13 @@ import re
 
 
 def get_address(record):
-    field = record.get_fields('260', '264')[0]
-    address = field['a']
-    return address.replace('[', '').replace(']', '').rstrip(' : ')
+    # https://www.loc.gov/marc/bibliographic/bd25x28x.html
+    fields = record.get_fields('260', '264')
+    if fields:
+        field = fields[0]
+        return field['a'].replace('[', '').replace(']', '').rstrip(': ')
+    else:
+        return None
 
 def get_author(record):
     # https://www.loc.gov/marc/bibliographic/bd1xx.html
@@ -14,27 +18,27 @@ def get_author(record):
     # https://www.loc.gov/marc/bibliographic/bd600.html
     # https://www.loc.gov/marc/bibliographic/bd800.html
     fields = record.get_fields('100', '110', '400', '600', '800')
-
     if fields:
         field = fields[0]
-        # In this case the subfield value is pronoun
+        # In this case the subfield value is a pronoun
         if field.tag == '400' and field.indicator2 == '1':
             rv = None
         else:
             value = field['a']
-            # Check if the subfield value ends with the initials
+            # Check if the subfield value ends with initials
             if re.findall(r'(?:[A-Z].)+$', value):
-                rv = value
+                rv = value.rstrip(',: ')
             else:
-                rv = value.rstrip('.,:')
+                rv = value.rstrip('.,: ')
             return rv
     else:
         return None
 
 def get_edition(record):
+    # https://www.loc.gov/marc/bibliographic/bd250.html
     field = record['250']
     if field:
-        return field['a']
+        return field['a'].rstrip('/= ')
     else:
         return None
 
@@ -46,13 +50,15 @@ def get_editor(record):
         return None
 
 def get_publisher(record):
+    # https://www.loc.gov/marc/bibliographic/bd25x28x.html
     publisher = record.publisher()
     if publisher:
-        return publisher.rstrip(',').rstrip(' ;')
+        return publisher.rstrip(',: ')
     else:
         return None
 
 def get_title(record):
+    # https://www.loc.gov/marc/bibliographic/bd245.html
     field = record['245']
     
     try:
@@ -77,6 +83,7 @@ def get_title(record):
     return rv.rstrip(' /')
 
 def get_year(record):
+    # https://www.loc.gov/marc/bibliographic/bd25x28x.html
     year = record.pubyear()
     if year:
         return year.lstrip('c').rstrip('.')
@@ -84,6 +91,7 @@ def get_year(record):
         return None
 
 def get_volume(record):
+    # https://www.loc.gov/marc/bibliographic/bd300.html
     field = record['300']
     if field:
         return field['a']
@@ -91,12 +99,11 @@ def get_volume(record):
         return None
 
 def get_pages(record):
+    # https://www.loc.gov/marc/bibliographic/bd300.html
     field = record['300']
-    p = re.search('([0-9]+) p.', field['a'])
-
-    if p:
-        pages = p.group(1)
-        return pages
+    if field:
+        p = re.search('([0-9]+) p.', field['a'])
+        return p.group(1) if p else None
     else:
         return None
 
@@ -104,6 +111,7 @@ def get_note(record):
     raise NotImplementedError
 
 def get_series(record):
+    # https://www.loc.gov/marc/bibliographic/bd490.html
     field = record['490']
     if field:
         return field['a'].rstrip(',')
