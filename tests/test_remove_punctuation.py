a2cb3dce29fdc8af04facc0e1ff05ed88d4dafef
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2022-08-27 12:35:13 +0300

    Update tests to reflect renaming of the function

--- tests/test_remove_punctuation.py
+++ tests/test_remove_punctuation.py
@@ -1,48 +1,59 @@
-from marc2bib import remove_punctuation
+from marc2bib import remove_records_punctuation
 
 
 def test_terminal_space_and_punctuation():
-    assert "Test" == remove_punctuation("Test .")
-    assert "Test" == remove_punctuation("Test ,")
-    assert "Test" == remove_punctuation("Test :")
-    assert "Test" == remove_punctuation("Test ;")
-    assert "Test" == remove_punctuation("Test +")
-    assert "Test" == remove_punctuation("Test /")
-    assert "Test" == remove_punctuation("Test =")
+    assert "Test" == remove_records_punctuation("Test .")
+    assert "Test" == remove_records_punctuation("Test ,")
+    assert "Test" == remove_records_punctuation("Test :")
+    assert "Test" == remove_records_punctuation("Test ;")
+    assert "Test" == remove_records_punctuation("Test +")
+    assert "Test" == remove_records_punctuation("Test /")
+    assert "Test" == remove_records_punctuation("Test =")
 
 
 def test_punctuation_without_space():
-    assert "Test" == remove_punctuation("Test.")
+    assert "Test" == remove_records_punctuation("Test.")
 
 
 def test_year():
-    assert "2022" == remove_punctuation("2022.")
+    assert "2022" == remove_records_punctuation("2022.")
 
 
 def test_initials():
-    assert "A.B." == remove_punctuation("A.B.")
-    assert "A.B." == remove_punctuation("A.B.,")
+    assert "A.B." == remove_records_punctuation("A.B.")
+    assert "A.B." == remove_records_punctuation("A.B.,")
 
 
 def test_name_suffixes():
-    assert "Jane Doe, Sr." == remove_punctuation("Jane Doe, Sr.")
-    assert "John Doe, Jr." == remove_punctuation("John Doe, Jr.")
+    assert "Jane Doe, Sr." == remove_records_punctuation("Jane Doe, Sr.")
+    assert "John Doe, Jr." == remove_records_punctuation("John Doe, Jr.")
 
 
 def test_ellipsis():
-    assert "Test..." == remove_punctuation("Test...")
+    assert "Test..." == remove_records_punctuation("Test...")
 
 
 def test_ordinal_numbers():
-    assert "1st." == remove_punctuation("1st.")
-    assert "2nd." == remove_punctuation("2nd.")
-    assert "3rd." == remove_punctuation("3rd.")
-    assert "4th." == remove_punctuation("4th.")
+    assert "1st." == remove_records_punctuation("1st.")
+    assert "2nd." == remove_records_punctuation("2nd.")
+    assert "3rd." == remove_records_punctuation("3rd.")
+    assert "4th." == remove_records_punctuation("4th.")
 
+    
+def test_strip_outer_square_brackets():
+    assert "Test" == remove_records_punctuation("[Test]", True)
+    assert "Test" == remove_records_punctuation("[Test].", True)
+    assert "One, [two]" == remove_records_punctuation("[One, [two]]", True)
 
+    
+def test_keep_outer_square_brackets():
+    assert "[Test]" == remove_records_punctuation("[Test]", False)
+
+    
 def test_common_abbreviation():
-    assert "ed." == remove_punctuation("ed.")
+    assert "ed." == remove_records_punctuation("ed.")
 
 
 def test_common_abbreviation_in_any_case():
-    assert "Co." == remove_punctuation("Co.")
+    assert "Co." == remove_records_punctuation("Co.")
+
